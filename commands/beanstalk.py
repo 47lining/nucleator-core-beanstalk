a7ba5c7702ea85f7e9fc55ab2a90ef0d3f3c5d87
Mark Chance <mark@peopledesigned.com>
2015-03-20 09:55:57 -0600

    move worker vs webserver diffs to vars

--- commands/beanstalk.py
+++ commands/beanstalk.py
@@ -83,7 +83,6 @@ class Beanstalk(Command):
         beanstalk_provision.add_argument("--customer", required=True, action=ValidateCustomerAction, help="Name of customer from nucleator config")
         beanstalk_provision.add_argument("--cage", required=True, help="Name of cage from nucleator config")
         beanstalk_provision.add_argument("--type", required=True, help="Type of beanstalk to provision (python or java)")
-        beanstalk_provision.add_argument("--tier", required=False, help="Tier of beanstalk to provision (webserver or worker)")
         beanstalk_provision.add_argument("--app_name", required=True, help="Name of beanstalk application to provision")
         beanstalk_provision.add_argument("--tier", required=False, help="Tier of beanstalk to provision (webserver or worker)")
         beanstalk_provision.add_argument("--beanstalk_instance_type", required=False, help="EC2 instance type to provision")
