a2ed3ae3674f9facfced5f87c0aa276b7641228d
Mark Chance <mark@peopledesigned.com>
2015-03-23 09:23:28 -0600

    Add nodejs beanstalk type

--- commands/beanstalk.py
+++ commands/beanstalk.py
@@ -28,12 +28,15 @@ class Beanstalk(Command):
         "python" : ("64bit Amazon Linux 2014.03 v1.0.7 running Python 2.7", 
                     "AWS Elastic Beanstalk Environment running Python Sample Application"),
         "java" :   ("64bit Amazon Linux 2014.09 v1.1.0 running Tomcat 8 Java 8",
-                     "AWS Elastic Beanstalk Environment running Java Sample Application")
+                     "AWS Elastic Beanstalk Environment running Java Sample Application"),
+        "nodejs" : ("64bit Amazon Linux 2014.09 v1.2.0 running Node.js",
+                    "AWS Elastic Beanstalk Environment running NodeJs Sample Application"),
     }
     
     sample_app_keys = {
         "python" : "basicapp.zip",
         "java" : "elasticbeanstalk-sampleapp.war"
+        "nodejs" : "nodejs-sample.zip"
     }
 
     def parser_init(self, subparsers):
